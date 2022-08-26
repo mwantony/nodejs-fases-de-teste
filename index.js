5f2a46919460991828e9cf3ca9f661b4d8eb4459
Antony Maziero Warken <againplayi7@gmail.com>
2022-08-26 14:48:16 -0300

    cap 2 finalizado

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
-console.log(somaHorasExtras(2000, 500));
+
+const calculaDescontos = (salario, descontos) => salario - descontos;
+
+export {
+  somaHorasExtras,
+  calculaDescontos,
+};
